738b00050ee39d83f5d84d93b0d3d44af787e0ca
Michael Veilleux <74661812+Mike-Veilleux@users.noreply.github.com>
2022-10-26 21:37:43 +0800

    initial commit

---
+++ Jest Testing/calculator.js
@@ -0,0 +1,38 @@
+const calculator = (num_1, num_2, operand) => {
+
+    if (typeof num_1 != 'number' || typeof num_2 != 'number') {
+        return "Error, you must use numbers!";
+    }
+
+    if (num_2 === 0 && operand === '/') {
+        return 'Error, cannot divide by 0 !'
+    }
+
+    switch (operand) {
+        case '+':
+            return num_1 + num_2;
+            break;
+
+        case '-':
+            return num_1 - num_2;
+            break;
+
+        case '*':
+            return num_1 * num_2;
+            break;
+
+        case '/':
+            return num_1 / num_2;
+            break;
+
+        default:
+            return 'Error, you must use a valid operator!';
+
+
+    }
+
+
+}
+module.exports = calculator;
+
+//console.log(calculator(5, "5", '/'));
---
+++ Jest Testing/calculator.test.js
@@ -0,0 +1,50 @@
+// https://humanwhocodes.com/snippets/2019/05/jest-globals-intellisense-visual-studio-code/
+
+const calculator = require('./calculator');
+
+
+describe("Calculate Numbers with valid operator (+ - * /)", () => {
+
+    it('Should Add two numbers', () => {
+        expect(calculator(5, 10, '+')).toBe(15)
+    })
+
+    it('Should Subtract two numbers', () => {
+        expect(calculator(10, 5, '-')).toBe(5)
+    })
+
+    it('Should Multiply two numbers', () => {
+        expect(calculator(5, 10, '*')).toBe(50)
+    })
+
+    it('Should Devide two numbers', () => {
+        expect(calculator(10, 5, '/')).toBe(2)
+    })
+
+});
+
+describe("Check if arguments are valid", () => {
+
+    it('First Arg should should be a numbers', () => {
+        expect(calculator("5", 10, '+')).toMatch(/Error, you must use numbers!/)
+    })
+
+    it('Second Arg should should be a numbers', () => {
+        expect(calculator(10, "5", '-')).toMatch(/Error, you must use numbers!/)
+    })
+
+    it('Third Arg should should be a valid operator', () => {
+        expect(calculator(10, 5, 'x')).toMatch(/Error, you must use a valid operator/)
+    })
+
+
+});
+
+describe("Check error with 0 as value", () => {
+
+    it('Error on divide by 0', () => {
+        expect(calculator(5, 0, '/')).toMatch(/Error, cannot divide by 0 !/)
+    })
+
+
+});
