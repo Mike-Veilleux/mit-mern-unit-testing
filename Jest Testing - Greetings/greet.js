30d6d9e4897f1055e1e0326e5afee90ac915a904
Michael Veilleux <74661812+Mike-Veilleux@users.noreply.github.com>
2022-10-27 22:07:19 +0800

    Refactored Greet function

--- Jest Testing - Greetings/greet.js
+++ Jest Testing - Greetings/greet.js
@@ -1,31 +1,26 @@
-// @ts-nocheck
 function Greet(name) {
 
-    let isArray = Array.isArray(name);
-    let isUpperCase;
-
-    if (!isArray) {
-        isUpperCase = name === name.toUpperCase();
+    if (typeof (name) === 'string') {
+        if (name === '') {
+            return 'Hello, there!';
+        } else if (name === name.toUpperCase()) {
+            return `HELLO, ${name}!`;
+        } else {
+            return `Hello, ${name}!`;
+        }
     }
 
-    if (name === "") {
-        return 'Hello, there!';
-    }
-    else if (!isArray && isUpperCase) {
-        return `HELLO, ${name}!`;
-    }
-    else if (isArray) {
+    if (Array.isArray(name) && name.length >= 1) {
         let str = 'Hello, ';
         for (let i = 0; i < name.length; i++) {
             str += `${name[i]}, `;
         }
         str = str.slice(0, -2);
         return `${str}!`;
+    } else {
+        return 'Hello, there!';
     }
-    else {
-        return `Hello, ${name}!`;
-    }
-}
 
+}
 
 module.exports = Greet;
--- Jest Testing - Greetings/greet.test.js
+++ Jest Testing - Greetings/greet.test.js
@@ -2,15 +2,23 @@ const Greet = require('./greet');
 
 describe('Testing greet function', () => {
 
-    it('Should return standard greeting when name is pass as argument', () => {
+    it('Should return standard greeting if there is no argument', () => {
+        expect(Greet()).toBe('Hello, there!')
+    })
+
+    it('Should return standard greeting if argument of type String is empty', () => {
+        expect(Greet('')).toBe('Hello, there!')
+    })
+
+    it('Should return standard greeting if argument of type Array is empty', () => {
+        expect(Greet([])).toBe('Hello, there!')
+    })
+
+    it('Should return personalized greeting when name is pass as argument', () => {
         expect(Greet('Mike')).toBe('Hello, Mike!')
     })
 
-    it('Should return standard greeting if argument is NULL', () => {
-        expect(Greet("")).toBe('Hello, there!')
-    })
-
-    it('Should return all upper case greeting if argument is all uppercase ', () => {
+    it('Should return all upper case greeting if argument of type string is all uppercase ', () => {
         expect(Greet('MIKE')).toBe('HELLO, MIKE!')
     })
 
@@ -18,7 +26,7 @@ describe('Testing greet function', () => {
         expect(Greet(['Mike', 'Kai'])).toBe('Hello, Mike, Kai!')
     })
 
-    it('Should handle an array of arbitrary number of name as input and greet all of the names', () => {
+    it('Should handle an array of arbitrary number of name as argument and greet all of the names', () => {
         expect(Greet(['Mike', 'Danielle', 'Eva', 'Kai'])).toBe('Hello, Mike, Danielle, Eva, Kai!')
     })
 
